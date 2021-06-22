bea87aaef6c54ad979c14255b85d61ca72a6c856
Michael Primavera <pastasauce59@gmail.com>
2021-06-22 10:59:11 -0400

    added daterange picker

--- client/src/components/DateReservation.js
+++ client/src/components/DateReservation.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 
 
 class DateReservation extends Component {
+
+    state = {
+        startDate: null,
+        endDate: null
+    }
+
     render() {
         return (
             <div className="room-reservation">
-                date picker going here...
+                <DateRangePicker
+                startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                />
             </div>
         );
     }
